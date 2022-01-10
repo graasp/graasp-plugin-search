2f1855ef333645d3b79903cd7e2595158868661f
Louise Wang <louise.w1997@gmail.com>
2022-01-10 20:34:48 +0100

    fix: remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,3 @@
-import { Actor } from 'graasp';
-
-export interface GraaspSearchPluginOptions {
-  publishedTagId: string;
-  graaspActor: Actor;
-}
-
 export enum Ranges {
   Title = 'title',
   Tag = 'tag',
